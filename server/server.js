ca569705c1afae92b4b79ab0b84faa6f7c13868d
alynxu <alynxu@outlook.com>
2022-11-30 16:22:03 -0800

    set ordersPart in server

--- server/server.js
+++ server/server.js
@@ -3,29 +3,39 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 const { join } = require("path");
 
-const mongoose = require('./config/dbconnect');
+const mongoose = require("./config/dbconnect");
 
-const user = require('./routes/user');
+const user = require("./routes/user");
+const order = require("./routes/order");
 
-const dotenv = require('dotenv')
-dotenv.config()
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 
-const bodyParser = require('body-parser');
-const { port } = require('./config/config');
+const bodyParser = require("body-parser");
+const { port } = require("./config/config");
 
 app.use(morgan("dev"));
 
-app.all('/*', function (req, res, next) {
+app.all("/*", function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
   next();
 });
 
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 1000000 }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 1000000,
+  })
+);
 
 app.use(
   helmet({
@@ -34,13 +44,16 @@ app.use(
 );
 
 app.use(express.static(join(__dirname, "build")));
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send("API is working properly");
-
 });
 
-app.use('/user', user);
+app.use("/user", user);
+app.use("/order", order);
 
-mongoose.connection.on('error', console.error.bind(console, 'MongoDB connetction error'));
+mongoose.connection.on(
+  "error",
+  console.error.bind(console, "MongoDB connetction error")
+);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
